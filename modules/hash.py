7c3404a2ab9dcbd5a3bd21b51fc5b0e1096c2bfe
Muhammed Musab Kaya <musabk2002@gmail.com>
2025-12-16 03:03:44 +0300

    feat: Integrity verification between two files.

--- modules/hash.py
+++ modules/hash.py
@@ -15,5 +15,15 @@ def hash_file(file_path):
             hash.update(chunk)
     return hash.hexdigest()
 
+def verify_integrity(file1, file2):
+    hash1 = hash_file(file1)
+    hash2 = hash_file(file2)
+    print("\nChecking integrity between", file1, " and", file2)
+    if hash1 == hash2:
+        return "File is intact. No modifications have been made."
+    return "File has been modified. Possibly corrupted or unsafe."
+
 if __name__ == "__main__":
     print("SHA Hash of file is: ", hash_file(r"sample_files\sample.txt"))
+    print(verify_integrity(r"sample_files\test1.txt", r"sample_files\test2.txt"))
+    print(verify_integrity(r"sample_files\test1.txt", r"sample_files\test3.txt"))
